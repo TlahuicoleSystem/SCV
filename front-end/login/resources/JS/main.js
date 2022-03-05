62e80204240cf9ba7bcfa25a79b64f3dc04fafc2
TlahuicoleSystem <barrales.josue.1i@gmail.com>
2022-03-05 12:20:29 -0600

    [TlahuicoleSystem] modulo de login/registro

---
+++ front-end/login/resources/JS/main.js
@@ -0,0 +1,9 @@
+function mostrar1() {
+    document.getElementById('registro').style.display = 'block';
+    document.getElementById('login').style.display = 'none';
+}
+
+function mostrar2() {
+    document.getElementById('registro').style.display = 'none';
+    document.getElementById('login').style.display = 'block';
+}
